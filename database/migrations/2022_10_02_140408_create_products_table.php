1a31cf0828c41a41e3969ce65648e9cb431b0962
Ajten <ajtencani@gmail.com>
2022-10-02 16:14:46 +0200

    setup products table migration

---
+++ database/migrations/2022_10_02_140408_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('supplier_id')->constrained('suppliers');
+            $table->foreignId('species_id')->constrained('species');
+            $table->foreignId('drying_method_id')->constrained('drying_methods');
+            $table->foreignId('treatment_id')->constrained('treatments')->nullable();
+            $table->foreignId('grade_option_id')->constrained('grade_options');
+            $table->integer('thickness');
+            $table->integer('width');
+            $table->integer('length');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
