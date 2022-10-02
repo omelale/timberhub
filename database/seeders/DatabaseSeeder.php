8a97285fae81a9ed2914eab8d32911b6181e7eae
Ajten <ajtencani@gmail.com>
2022-10-02 16:34:54 +0200

    specify order of execution of seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            DryingMethodSeeder::class,
+            TreatmentSeeder::class,
+            SpecieSeeder::class,
+            SupplierSeeder::class,
+            GradeSeeder::class,
+            GradeOptionSeeder::class,
+        ]);
     }
 }
