8da44f1e858416b36617e63caf8be66c0d6e7114
Ajten <ajtencani@gmail.com>
2022-10-03 19:32:52 +0200

    added link to products page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Buy sawn timber faster and smarter</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Order materials quickly, free up cash and increase margins. Timberhub saves you time and money by managing procurement, finance and transportation.</p>
-                <a href="#" class="focus:outline-none text-white bg-green-700 px-5 py-3 mr-3 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 ">
-                    Get the free quote
+                <a href="{{route('products.index')}}" class="focus:outline-none text-white bg-green-700 px-5 py-3 mr-3 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 ">
+                    Browse our products
                 </a>
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
