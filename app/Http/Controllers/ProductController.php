e6ece9f0720b2e5c888dbbd36c42615f9dcf806a
Ajten <ajtencani@gmail.com>
2022-10-03 23:26:57 +0200

    validate and save product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -55,7 +55,36 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $species = Specie::all();
+        $speciesIds = $species->pluck('id');
+        $treatments = Treatment::all();
+        $treatmentIds = $treatments->pluck('id');
+        $grades = Grade::all();
+        $gradeIds = $grades->pluck('id');
+        $gradeOptions = GradeOption::all();
+        $gradeOptionIds = $gradeOptions->pluck('id');
+        $dryingMethods = DryingMethod::all();
+        $dryingMethodIds = $dryingMethods->pluck('id');
+        $validated = $request->validate([
+            'species' => 'required|in:' . $speciesIds->join(','),
+            'dryingMethod' => 'required|in:' . $dryingMethodIds->join(','),
+            'grade' => 'required|in:' . $gradeOptionIds->join(','),
+            'thickness' => 'required|numeric|gt:0',
+            'width' => 'required|numeric|gt:0',
+            'length' => 'required|numeric|gt:0',
+            'treatment' => 'nullable|in:' . $treatmentIds->join(','),
+        ]);
+        // check if i should save like this or save through relationship
+        $product = new Product();
+        $product->species_id = $validated['species'];
+        $product->drying_method_id = $validated['dryingMethod'];
+        $product->grade_option_id = $validated['grade'];
+        $product->thickness = $validated['thickness'];
+        $product->width = $validated['width'];
+        $product->length = $validated['length'];
+        // $product->treatment_id = $validated['treatment'] !== null ? $validated['treatment'] : null;
+        $product->save();
+        return redirect()->route('products.index');
     }
 
     /**
