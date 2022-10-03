87cf0f1433ca5c581c0676d4fe413e26a88730b1
Ajten <ajtencani@gmail.com>
2022-10-03 19:54:36 +0200

    show if missing products and add link to create page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,16 +1,28 @@
 @extends('layouts.app')
 @section('title', 'Products')
 @section('content')
-    <h1 class="text-3xl font-bold underline">
-        Products
-    </h1>
-    <ul>
-        @foreach ($products as $product)
-            <li>
-                <a href="{{ route('products.show', $product) }}">
-                    {{ $product->name }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container-fluid mt-5">
+        @if (count($products) > 0)
+            <h1 class="text-3xl font-bold underline">
+                Browse our products
+            </h1>
+            <ul>
+                @foreach ($products as $product)
+                    <li>
+                        <a href="{{ route('products.show', $product) }}">
+                            {{ $product->name }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <div class="container mx-auto ">
+                <h2 class="text-3xl mb-4 font-bold ">
+                    There are currently no products available
+                </h2>
+                <a href="{{ route('products.create') }}" class="focus:outline-none text-white bg-green-700 px-5 py-3 mr-3 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Create a new product now</a>
+            </div>
+        @endif
+    </div>
+
 @endsection
