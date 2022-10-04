cb133cacd3dd9393e21b37915cf3d123ef65a218
Ajten <ajtencani@gmail.com>
2022-10-04 13:49:36 +0200

    fix method type

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -7,8 +7,9 @@
             <h1 class="text-3xl font-bold underline">
                 Edit product
             </h1>
-            <form action="{{ route('products.update', ['product' => $product]) }}" class="mt-5" method="patch">
+            <form action="{{ route('products.update', ['product' => $product]) }}" class="mt-5" method="POST">
                 @csrf
+                {{ method_field('PATCH') }}
                 <div class="mb-6">
                     <label for="species" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Species</label>
                     <select id="species" name="species" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
