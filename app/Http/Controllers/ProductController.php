51801f64f88f1b04f16c1dda331db92ca423464d
Ajten <ajtencani@gmail.com>
2022-10-04 11:05:12 +0200

    optimization of query when listing all products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,7 +22,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::all();
+        $products = Product::with('species', 'dryingMethod', 'treatment', 'gradeOption')->get();
         return view('products.index', compact('products'));
     }
 
