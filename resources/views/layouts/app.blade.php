2bcab0df1d7bc69b17dd1d6d2c2c7fb4c4da402c
Ajten <ajtencani@gmail.com>
2022-10-04 21:03:01 +0200

    display session error messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,11 @@
                     <span class="font-medium">{{ session('message') }}</span>
                 </div>
             @endif
+            @if (session()->has('error'))
+                <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+                    <span class="font-medium">{{ session('error') }}</span>
+                </div>
+            @endif
             @yield('content')
         </div>
         @include('partials.footer')
